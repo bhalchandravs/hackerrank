0d67d27fca27da1df19f136b20c5db252b2dfae1
bhalchandravs <88782206+bhalchandravs@users.noreply.github.com>
2021-09-21 11:44:48 +0530

    feat: Added another solution using for loop

--- Java-end-of-file.java
+++ Java-end-of-file.java
@@ -8,11 +8,16 @@ public class Solution {
 
     public static void main(String[] args) {
       Scanner scan = new Scanner(System.in);
+      //Solution using while loop
       int i = 0;
       while(scan.hasNext()){
            i++;
        System.out.println(i + " " + scan.nextLine());
       }
       
+        //Solution using for loop
+        for(int i=1;scan.hasNext();i++){
+        System.out.println(i + " " + scan.nextLine());
+        
     }
 }
